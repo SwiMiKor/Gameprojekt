ee0abdb631b566c64bc134d2888fe6b40ecac4db
Lukas Wider <Lukas.Wider@bwz-rappi.ch>
2022-01-03 11:57:57 +0100

    Model Source-Code

---
+++ src/model/enemy.java
@@ -0,0 +1,11 @@
+package model;
+
+public abstract class Enemy {
+	private int score;
+	private int life;
+	private int color;
+    private float x;
+    private float y;
+    private int size = 50;
+    private int damage;
+}
