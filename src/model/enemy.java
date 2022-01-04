667220b887942818882956bc02e72e4ba5a2b6fa
Lukas Wider <Lukas.Wider@bwz-rappi.ch>
2022-01-04 11:55:39 +0100

    These are the enemys of mankind

--- src/model/enemy.java
+++ src/model/enemy.java
@@ -1,11 +1,12 @@
 package model;
 
-public abstract class Enemy {
+public  class Enemy  extends Person{
 	private int score;
-	private int life;
-	private int color;
-    private float x;
-    private float y;
-    private int size = 50;
     private int damage;
+    private float speed = 10;
+    
+	public Enemy(float x, float y, int speed, int size, int color, int life) {
+		super(x, y, speed, size, color, life);
+
+	}
 }
