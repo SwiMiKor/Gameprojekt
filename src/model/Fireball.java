359df328a2342b40a5ac65f1818ae5283b69bce1
Lukas Wider <Lukas.Wider@bwz-rappi.ch>
2022-01-24 19:13:21 +0100

    Feuerballklasse...Geschoss des Shooters

--- src/model/Fireball.java
+++ src/model/Fireball.java
@@ -1,45 +1,55 @@
 package model;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
-
-public class Fireball extends Shooter{
+/**
+ * Feuerball ist das Projektil welches vom Shooter verschossen wird
+ * @author Lukas
+ *
+ */
+public class Fireball extends Person{
+	private boolean broken;
 	
-	private float x;
-    private float y;
-	private int speed = 10;
-	private int size = 50;
-	private int color;
-	
-	Direction dir = Direction.N;
-	
-	
-	public enum Direction {
-        N,E,S,W;
-    }
-	
-	
-
-	
-	public Fireball(float x, float y, int speed, int size, int life, int color, model.Shooter.Direction direction) {
-		super(x, y, speed, size, life, color, direction);
 		
-	}
-
-
-	public void drawFireball(PApplet window) {
-		window.stroke(0);
-		window.fill(color);
-		window.circle(5, 5, 5);
-	}
-
-
-    public void move(PApplet window) {
-        switch(dir) {
-            case N: y = Math.max(0, y - speed); break;
-            case E: x = Math.min(window.width, x + speed); break;
-            case S: y = Math.min(window.height, y + speed); break;
-            case W: x = Math.max(0, x - speed);
-        }
-    }
+		/**
+		 * 
+		 * @param speed
+		 * @param width
+		 * @param height
+		 * @param x
+		 * @param y
+		 * @param direction
+		 * @param window
+		 */
+		public Fireball(int speed, int width, int height, int x, int y, Direction direction, PApplet window) {
+		super(speed, width, height, x, y, direction);
+		
+		broken = false;
+		setImg_E(window.loadImage("/resources/fireballfr.png"));
+		setImg_W(window.loadImage("/resources/fireballfl.png"));
+	
+}
+		/**
+		 * zeichnet den Feuerball
+		 */
+		public void draw(PApplet window) {
+			window.noStroke();
+			window.noFill();
+			window.rect(getX(), getY(),140, 90);
+			
+			PImage img = null;
+			switch (getDirection()) {
+			case E: img = getImg_E(); break; 
+			case W: img = getImg_W(); break;
+			}
+			window.image(img, getX(), getY());
+			
+		}
+		public boolean isBroken() {
+			return broken;
+		}
+		public void setBroken(boolean broken) {
+			this.broken = broken;
+		}
 }
