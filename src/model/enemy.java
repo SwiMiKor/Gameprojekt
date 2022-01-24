74e990c70f4d90840eda7c22bded9d4a95b1dafe
Lukas Wider <Lukas.Wider@bwz-rappi.ch>
2022-01-24 19:14:39 +0100

    Enemyklasse

--- src/model/enemy.java
+++ src/model/enemy.java
@@ -1,36 +1,95 @@
 package model;
 
-public  class Enemy  extends Person{
-	private int score;
-    private int damage;
-    
-    
-	public Enemy(float x, float y, int speed, int size, int color, int life) {
-		super(x, y, speed, size, color, life);
+/**
+ * Enemy erbt von Person und ist die Elterklasse von Paladin und Knight
+ * @author Lukas
+ *
+ */
+public abstract class Enemy extends Person{
+	private boolean broken;
+	private int speed;
+	public int x;
+	public int y;	
+	
+	/**
+	 * 
+	 * @param broken
+	 * @param speed
+	 * @param width
+	 * @param height
+	 * @param x
+	 * @param y
+	 * @param direction
+	 */
+	public Enemy(boolean broken, int speed, int width, int height, int x, int y, Direction direction) {
+		super(speed, width, height, x, y, direction);
+		this.broken = false;
+		this.speed = speed;
+		this.x = x;
+		this.y = y;
+	}
+
+
+
+	/**
+	 * diese Methode bewegt die Gegner
+	 */
+	public void move(){
+    	switch(direction) {
+    	case E: x += speed;break;
+    	case W: x -= speed;
+    	
+    	}
+	}
 
 	
+	public boolean isBroken() {
+		return broken;
+	}
+
 	
+	public void setBroken(boolean broken) {
+		this.broken = broken;
 	}
 
-	public Enemy(float x, float y, int speed, int size, int color, int life, int score, int damage, float speed2) {
-		super(x, y, speed, size, color, life);
-		this.setScore(score);
-		this.setDamage(damage);
+
+	public int getSpeed() {
+		return speed;
 	}
 
-	public int getDamage() {
-		return damage;
+
+
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
 	}
 
-	public void setDamage(int damage) {
-		this.damage = damage;
+
+
+
+	public int getX() {
+		return x;
 	}
 
-	public int getScore() {
-		return score;
+
+
+
+	public void setX(int x) {
+		this.x = x;
 	}
 
-	public void setScore(int score) {
-		this.score = score;
+
+
+
+	public int getY() {
+		return y;
 	}
+
+
+
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
 }
